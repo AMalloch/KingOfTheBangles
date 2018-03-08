36cf128f266ef98364207a89293e6f8c6c992191
Jim Bevington <jim.bevington@hotmail.com>
2018-03-08 11:13:43 +0000

    setup Player fields and constructor

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,15 @@
-public class Player {
+public abstract class Player {
+
+    private String name;
+    private int health;
+    private ArrayList<Item> inventory;
+    private Item selectedItem;
+    private int satchel;
+
+    public Player(String name, int health, ArrayList<Item> inventory) {
+        this.name = name;
+        this.health = health;
+        this.inventory = inventory;
+        this.selectedItem = this.inventory.get(0);
+    }
 }
