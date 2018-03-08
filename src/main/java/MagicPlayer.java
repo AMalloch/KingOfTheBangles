703c11ac3857fb73bbf01326fb404da3b12b137a
Jim Bevington <jim.bevington@hotmail.com>
2018-03-08 11:30:17 +0000

    added MagicPlayer constructor

--- src/main/java/MagicPlayer.java
+++ src/main/java/MagicPlayer.java
@@ -1,2 +1,10 @@
-public class MagicPlayer {
+import java.util.ArrayList;
+
+public abstract class MagicPlayer extends Player{
+    private Creature creature;
+
+    public MagicPlayer(String name, int health, ArrayList<Item> inventory, Creature creature) {
+        super(name, health, inventory);
+        this.creature = creature;
+    }
 }
